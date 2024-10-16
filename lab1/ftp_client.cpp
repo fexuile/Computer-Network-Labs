9c7507e4d406388ea651b37dd72f2b5735e40c8f
fexuile <fexuile@outlook.com>
2024-10-16 18:33:53 +0800

    2024/10/16 18:33 finished

--- lab1/ftp_client.cpp
+++ lab1/ftp_client.cpp
@@ -16,7 +16,6 @@ void open() {
         return;
     }
     std::cin >> ip >> port;
-    std::cout << ip <<" "<< port << std::endl;
     sockid = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
     addr.sin_port = htons(port);
--- lab1/ftp_server.cpp
+++ lab1/ftp_server.cpp
@@ -4,13 +4,15 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include "helper.h"
+#include <filesystem>
+
 int port;
 char ip[16];
 
 struct Info {
     int client;
     pthread_t thread;
-    char dir[MAX_LINE_LENGTH];
+    std::string dir;
 }info[MAX_THREAD_COUNTS];
 
 void* doit(void *args) {
@@ -36,24 +38,40 @@ void* doit(void *args) {
             struct message reply = LS_CONN_REPLY;
             int length;
             FILE *tmp;
-            tmp = popen("ls", "r");
-            static char buffer[MAX_LINE_LENGTH];
+            std::string cmdline = "ls " + info->dir;
+            tmp = popen(cmdline.c_str(), "r");
+            char *buffer = (char *)malloc(MAX_LINE_LENGTH * 8);
             length = fread(buffer, 1, MAX_LINE_LENGTH, tmp);
             pclose(tmp);
             buffer[length++] = '\0';
             reply.m_length = htonl(length + 12);
             Send(client, &reply, 12, 0);
-            Send(client, &buffer, length, 0); 
+            Send(client, buffer, length, 0); 
+            free(buffer);
         }
         else if(msg.m_type == 0xA5) {
-            
+            struct message reply = CHANGE_DIR_REPLY;
+            int length = ntohl(msg.m_length) - 12;
+            char *dirname = (char *)malloc(length << 2);
+            std::string cmdline;
+            Recv(client, dirname, length, 0);
+            cmdline = info->dir + "/" + std::string(dirname);
+            if(std::filesystem::exists(std::string(cmdline)) == -1)
+                reply.m_status = 0;
+            else {
+                reply.m_status = 1;
+                info->dir = cmdline;
+            }
+            Send(client, &reply, 12, 0);
+            free(dirname);
         }
         else if(msg.m_type == 0xA7) {
             struct message reply = GET_REPLY;
             int length = ntohl(msg.m_length) - 12;
-            static char filename[MAX_LINE_LENGTH];
-            Recv(client, &filename, length, 0);
-            FILE *file = fopen(filename, "r");
+            char *filename = (char *)malloc(length << 2);
+            Recv(client, filename, length, 0);
+            std::string cmdline = info->dir + "/" + filename;
+            FILE *file = fopen(cmdline.c_str(), "r");
             if(file == NULL) {
                 reply.m_status = 0;
                 Send(client, &reply, 12, 0);
@@ -67,36 +85,42 @@ void* doit(void *args) {
                 rewind(file);
                 reply.m_length = htonl(length + 12);
                 Send(client, &reply, 12, 0);
-                static char buffer[FILE_MAX_SIZE];
+                char *buffer = (char *)malloc(FILE_MAX_SIZE << 2);
                 length = fread(buffer, 1, FILE_MAX_SIZE, file);
-                Send(client, &buffer, length, 0);
+                Send(client, buffer, length, 0);
             }
             fclose(file);
+            free(filename);
         }
         else if(msg.m_type == 0xA9) {
             struct message reply = PUT_REPLY;
             int length = ntohl(msg.m_length) - 12;
-            static char filename[MAX_LINE_LENGTH];
-            Recv(client, &filename, length, 0);
+            char *filename = (char *)malloc(sizeof(char) * (length << 1));
+            Recv(client, filename, length, 0);
             Send(client, &reply, 12, 0);
-            FILE *file = fopen(filename, "wb");
+            std::string cmdline = info->dir + "/" + std::string(filename);
             Recv(client, &msg, 12, 0);
             if(memcmp(msg.m_protocol, protocol, MAGIC_NUMBER_LENGTH) || msg.m_type != FILE_DATA.m_type) {
                 puts("Error in file transfer.");
-                return NULL;
+                free(filename);
+                pthread_exit(NULL);
             }
             length = ntohl(msg.m_length) - 12;
-            static char buffer[FILE_MAX_SIZE];
-            Recv(client, &buffer, length, 0);
+            FILE *file = fopen(cmdline.c_str(), "wb");
+            char* buffer = (char *)malloc(FILE_MAX_SIZE << 2);
+            Recv(client, buffer, length, 0);
             fwrite(buffer, 1, length, file);
             fclose(file);
+            free(buffer);
+            free(filename);
         }
         else if(msg.m_type == 0xAB) {
             struct message reply = SHA256_REPLY;
             int length = ntohl(msg.m_length) - 12;
-            static char filename[MAX_LINE_LENGTH];
-            Recv(client, &filename, length, 0);
-            FILE *file = fopen(filename, "r");
+            char *filename = (char *)malloc(length << 2);
+            Recv(client, filename, length, 0);
+            std::string cmdline = info->dir + "/" + filename;
+            FILE *file = fopen(cmdline.c_str(), "r");
             if(file == NULL) {
                 reply.m_status = 0;
                 Send(client, &reply, 12, 0);
@@ -109,15 +133,16 @@ void* doit(void *args) {
                 std::string cmd_str;
                 cmd_str = "sha256sum " + std::string(filename);
                 tmp = popen(cmd_str.c_str(), "r");
-                static char buffer[FILE_MAX_SIZE];
+                char* buffer = (char *)malloc(FILE_MAX_SIZE << 2);
                 length = fread(buffer, 1, FILE_MAX_SIZE, tmp);
                 pclose(tmp);
                 buffer[length++] = '\0';
                 reply.m_length = htonl(length + 12);
                 Send(client, &reply, 12, 0);
-                Send(client, &buffer, length, 0);
+                Send(client, buffer, length, 0);
+                free(buffer);
             }
-            fclose(file);
+            fclose(file);free(filename);
         }
         else if (msg.m_type == 0xAD) {
             struct message reply = QUIT_CONN_REPLY;
@@ -159,6 +184,7 @@ int main(int argc, char *argv[]) {
                 id = i;
                 break;
             }
+        info[id].dir = ".";
         pthread_create(&info[id].thread, NULL, doit, (void *)&info[id]);
         pthread_detach(info[id].thread); 
     }
